7da802b56cb5350da1d3ad94f5944de94f6d06f8
Honza <bla@bla>
2024-01-30 00:35:35 +0100

    zakladni nastaveni

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Deployed
-      </header>
+    <div className='container'>
+      <h1>React App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda totam accusamus omnis doloremque, voluptatem unde nemo. Explicabo adipisci est ipsam rerum esse. Error quis beatae itaque dolorum ullam, maiores eum.</p>
     </div>
   );
 }
